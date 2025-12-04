4d4a466b9479e669b7670be650dec3923f8dee36
vishalsaraha <vishalsaran23@gmail.com>
2025-12-04 21:34:25 +0530

    building home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,34 +1,35 @@
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Image from 'next/image';
 
 
-export default function HomePage() {
+export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid size={4}>
-         <Box sx={{
+          <Box sx={{
             backgroundColor: 'black',
-         }}>
-             
-         </Box>
+           
+          }}>
+          </Box>
         </Grid>
         <Grid size={6}>
-            <Box>
-             <Image 
-                src="/images/typewriter.png" 
-                alt="type writer image" 
-                width={120}
-                height={80} 
-              />
-         </Box>
+          <Box sx={{
+            alignItems: "right"
+          }}>
+            <Image 
+              src="/images/typewriter.png" 
+              alt="type writer image" 
+              width={600}
+              height={600} 
+            draggable={false} //stop image from dragging on to other thing
+            />
+          </Box>
         </Grid>
-        <Grid size={4}>
-          
-        </Grid>
-        <Grid size={8}>
-         
+        <Grid size={2}>
+          {/* Empty Grid for layout */}
         </Grid>
       </Grid>
     </Box>
